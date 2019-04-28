98bed7890216dc6fdfc9ec625224bf9296580268
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-04-28 16:51:20 +0200

    fetches addad and adds search input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [pics, setPics] = useState(null);
+  const [searchTerm, setSerchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breed/${searchTerm}/images`)
+      .then(res => res.json())
+      .then(res => setPics(res.message))
+  }, [searchTerm])
+
+  console.log(pics, searchTerm)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="">
+        find the doggie
+        <input type="text"
+          value={searchTerm}
+          onChange={e => setSerchTerm(e.target.value)} />
+      </label>
+      <ul>
+        {pics !== null ? (pics.map((el, index) => (
+          <li key={index}>
+            <img src={el}></img>
+          </li>
+        ))) : null}
+      </ul>
     </div>
   );
 }
