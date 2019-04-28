e48e7768483d76ce5d4d7b928dcb9f36e0d08e48
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-04-28 19:16:17 +0200

    adds search on button and initial error handling

--- src/App.js
+++ src/App.js
@@ -3,31 +3,40 @@ import './App.css';
 
 function App() {
 
-  const [pics, setPics] = useState(null);
-  const [searchTerm, setSerchTerm] = useState("");
+  const [pics, setPics] = useState([]);
+  const [query, setQuery] = useState("");
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
-    fetch(`https://dog.ceo/api/breed/${searchTerm}/images`)
+    fetch(`https://dog.ceo/api/breed/${search}/images`)
+      .then(res => {
+        if (!res.ok) {
+          throw Error(res.statusText);
+        }
+        return res;
+      })
       .then(res => res.json())
       .then(res => setPics(res.message))
-  }, [searchTerm])
-
-  console.log(pics, searchTerm)
+      .catch(err => console.log(err))
+  }, [search])
 
   return (
     <div className="App">
       <label htmlFor="">
         find the doggie
         <input type="text"
-          value={searchTerm}
-          onChange={e => setSerchTerm(e.target.value)} />
+          value={query}
+          onChange={e => setQuery(e.target.value)} />
       </label>
+      <button onClick={() => setSearch(query)}>
+        search
+      </button>
       <ul>
-        {pics !== null ? (pics.map((el, index) => (
+        {pics.map((el, index) => (
           <li key={index}>
             <img src={el}></img>
           </li>
-        ))) : null}
+        ))}
       </ul>
     </div>
   );
