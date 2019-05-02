8f3935efecd9a599e6a502b02ef4cb14128d19d7
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-05-02 18:53:25 +0200

    adds adding to favs and show favs on hover needs styling & division into components & local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faPaw } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
+
+library.add(faHeart, faPaw);
 
 const App = () => {
 
@@ -8,6 +13,8 @@ const App = () => {
   const [search, setSearch] = useState("");
   const [isLoading, setLoading] = useState(false);
   const [isError, setError] = useState(false);
+  const [favourites, setFavourite] = useState([]);
+  const [mouseIsOver, setMouseIsOver] = useState(false);
 
   useEffect(() => {
     if (search) {
@@ -32,8 +39,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="search--wrapper">
-        <label htmlFor="search-term" className="search__label"> find the doggie </label>
+      <div className="search-wrapper">
+        <label htmlFor="search-term" className="search__label">find doggie</label>
         <input type="text"
           name="search-term"
           id="search-name"
@@ -43,14 +50,26 @@ const App = () => {
         <button className="search__button" onClick={() => setSearch(query)}>search</button>
       </div>
       {isLoading && <div className="lds-heart"><div></div></div>}
-      {isError && <div className="data-error">Something went wrong</div>}
+      {isError && <div className="data-error">Ooops, no such doggie in our database :(</div>}
       <ul className="doggies-list">
         {pics.map((el, index) => (
           <li key={index} className="doggie">
-            <img src={el} className="doggie__img"></img>
+            <img src={el} className="doggie__img" alt=""></img>
+            <FontAwesomeIcon icon="heart" className="doggie__heart" onClick={() => setFavourite([...favourites, el].filter((el, i, arr) => arr.indexOf(el) === i))} />
           </li>
         ))}
       </ul>
+      <div className="favourite-doggies-wrapper">
+        <div className="favourite-doggies__icon" onMouseEnter={() => setMouseIsOver(!mouseIsOver)} onMouseLeave={() => setMouseIsOver(!mouseIsOver)} > HOVER</div>
+        <FontAwesomeIcon icon="paw" />
+        <ul className="favourite-doggies__list">
+          {mouseIsOver && favourites.map((el, index) => (
+            <li key={index}>
+              <img src={el} alt="" />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
